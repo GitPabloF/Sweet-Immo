30b55fa54efd0413b5e72c5f0b6b5dcf8d307472
UserWindowsXP <wwwebsurf@gmail.com>
2023-07-03 14:56:04 +0200

    slider carousel ok

--- src/pages/accommodation/accommodation.jsx
+++ src/pages/accommodation/accommodation.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { datas } from '../../datas/datas'
 import './Accommodation.scss'
 import Rating from '../../components/Rating/Rating'
@@ -19,37 +19,58 @@ export default function Accommodation() {
     // based on the (1 to 5) rating return the gray class for the star if needed
     const dataRating = data.rating
     const star5 = dataRating === '5' ? '' : 'rating_star_grey'
-    const star4 = dataRating === '5' ||  dataRating === '4' ? '' : 'rating_star_grey'
-    const star3 = dataRating === '5' ||  dataRating === '4' ||  dataRating === '3' ? '' : 'rating_star_grey'
-    const star2 = dataRating === '5' ||  dataRating === '4' ||  dataRating === '3' ||  dataRating === '2' ? '' : 'rating_star_grey'
-    const star1 = dataRating === '5' ||  dataRating === '4' ||  dataRating === '3' ||  dataRating === '2' ||  dataRating === '1' ? '' : 'rating_star_grey'
+    const star4 =
+        dataRating === '5' || dataRating === '4' ? '' : 'rating_star_grey'
+    const star3 =
+        dataRating === '5' || dataRating === '4' || dataRating === '3'
+            ? ''
+            : 'rating_star_grey'
+    const star2 =
+        dataRating === '5' ||
+        dataRating === '4' ||
+        dataRating === '3' ||
+        dataRating === '2'
+            ? ''
+            : 'rating_star_grey'
+    const star1 =
+        dataRating === '5' ||
+        dataRating === '4' ||
+        dataRating === '3' ||
+        dataRating === '2' ||
+        dataRating === '1'
+            ? ''
+            : 'rating_star_grey'
 
     // to map a list of the equipments
     const dataEquipments = data.equipments.map((equipment, index) => {
-        return <li key={`${equipment}${index}-`}className="accommodation_equipments_li">{equipment}</li>
+        return (
+            <li
+                key={`${equipment}${index}-`}
+                className="accommodation_equipments_li"
+            >
+                {equipment}
+            </li>
+        )
     })
 
-    // to map the images
-    const dataImg = data.pictures.map((picture, i) => {
-        return <img class='accommodation_img' key={`accommodation_img-${i+1}`} id={`accommodation_img-${i+1}`} src={picture} alt="" />
-    })
-    
-    
-    // const [imgStatus, ]
-    //  function goToNext(){
-    //     imgStatus++
-    //     console.log(imgStatus)
-    //  } 
-    
+    const [imgStatus, updateImgStatus] = useState(0)
+
+    const  goToNext = () => imgStatus >= data.pictures.length -1 ? updateImgStatus(1) : updateImgStatus( imgStatus + 1) 
+    const  goToPrevious = () => imgStatus <= 0 ? updateImgStatus(data.pictures.length -1) : updateImgStatus( imgStatus - 1) 
 
     return (
         <div className="accommodation">
             <div className="accommodation_img">
-                {dataImg}
-                <span>
+                <img
+                    class="accommodation_img"
+                    id={`accommodation_img-${imgStatus + 1}`}
+                    src={data.pictures[imgStatus]}
+                    alt=""
+                />
+                <span onClick={goToPrevious}>
                     <i className="fa-solid fa-angle-left"></i>
                 </span>
-                <span >
+                <span onClick={goToNext}>
                     <i className="fa-solid fa-angle-right"></i>
                 </span>
             </div>
@@ -64,19 +85,16 @@ export default function Accommodation() {
                     <div className="accommodation_host">
                         <span>{data.host.name}</span>
                         <div className="accommodation_host_img">
-                            <img
-                                src={data.host.picture}
-                                alt={data.host.name}
-                            />
+                            <img src={data.host.picture} alt={data.host.name} />
                         </div>
                     </div>
 
                     <div className="accommodation_rating">
-                        <Rating isGrey={star1}/>
-                        <Rating isGrey={star2}/>
-                        <Rating isGrey={star3}/>
-                        <Rating isGrey={star4}/>
-                        <Rating isGrey={star5}/>
+                        <Rating isGrey={star1} />
+                        <Rating isGrey={star2} />
+                        <Rating isGrey={star3} />
+                        <Rating isGrey={star4} />
+                        <Rating isGrey={star5} />
                     </div>
                 </div>
             </div>
