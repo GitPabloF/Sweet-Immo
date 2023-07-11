859516c31bf189029e1b124c24f17cf5ceb63095
UserWindowsXP <wwwebsurf@gmail.com>
2023-07-11 12:09:55 +0200

    Redirection to 404 when ID does not exist ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { datas } from './datas/datas'
 import GlobalStyle from './utilis/style/GlobalStyle'
-import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home/Home'
 import About from './pages/About/About';
@@ -11,18 +9,6 @@ import Error from './pages/Error/Error';
 import Accommodation from './pages/Accommodation/Accommodation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// const datasID = datas.map((data) => {
-//   return(
-//   )})
-  
-//   const isIDExist = `/accommodation/:` !== datasID.includes()
-
-// const t = datas.map((data) => {
-//     return(
-//       `/accommodation/${data.id}`
-//     )})
-// // '/accommodation/:id'
 
 root.render(
   <React.StrictMode>
--- src/pages/accommodation/accommodation.jsx
+++ src/pages/accommodation/accommodation.jsx
@@ -9,182 +9,140 @@ import Collapse from '../../components/Collapse/Collapse'
 export default function Accommodation() {
     let { id } = useParams()
     const navigate = useNavigate()
-    const [accommodation, setAccommodation] = useState({})
+    const [accommodationData, setAccommodationData] = useState(null)
 
-    // TRY TO return to error page if id doesn't exist
-    // const datasID = datas.map(data => data.id)
-    // console.log(datasID[0])
-
+    // redirect to error page if 'id' doesn't exist and store Data into State
     useEffect(() => {
         loadData(id)
-    }, [])
+    }, [id])
+
+    let datasID = datas.map((data) => data.id)
 
     function loadData(id) {
-        let found = false;
-        datas.map((data, i) => {
-            if (data.id === id) {
-                setAccommodation(data)
-                found = true
-                console.log('if', data.id)
-                return
-            }
-        })
-        if (!found) {
-            console.log('no', accommodation)
-            navigate('/')
+        if (!datasID.includes(id)) {
+            navigate('*')
+        } else {
+            const newData = datas.find((item) => item.id === id)
+            setAccommodationData(newData)
         }
     }
 
-    // for(let i = 0; i < datasID.length ; i++ ){
-    //     let number = 0
-    //     if(id === datasID[i]){
-    //         number++
-    //     }
-    //     console.log(number)
-    //     if(number === 0){
-    //         window.open('/')
-    //     }
-    // }
+    //  map the tags of the accommodation
+    function renderTags() {
+        return accommodationData.tags.map((tag, i) => {
+            return <span key={`tag${i}`}>{tag}</span>
+        })
+    }
 
-    // to get all the datas of the element where the id is the present
-    const data = datas.find((item) => item.id === id)
+    // map a list of the equipments
+    function renderEquipments() {
+        return accommodationData.equipments.map((equipment, index) => {
+            return (
+                <li
+                    key={`${equipment}${index}-`}
+                    className="accommodation_equipments_li"
+                >
+                    {equipment}
+                </li>
+            )
+        })
+    }
 
-    // to map the tags of the accommodation
-    const dataTags = data.tags.map((tag) => {
-        return <span>{tag}</span>
-    })
-
-
-    // based on the (1 to 5) rating return the gray class for the star if needed
-    const dataRating = data.rating
-    const star5 = dataRating === '5' ? '' : 'rating_star_grey'
-    const star4 =
-        dataRating === '5' || dataRating === '4' ? '' : 'rating_star_grey'
-    const star3 =
-        dataRating === '5' || dataRating === '4' || dataRating === '3'
-            ? ''
-            : 'rating_star_grey'
-    const star2 =
-        dataRating === '5' ||
-        dataRating === '4' ||
-        dataRating === '3' ||
-        dataRating === '2'
-            ? ''
-            : 'rating_star_grey'
-    const star1 =
-        dataRating === '5' ||
-        dataRating === '4' ||
-        dataRating === '3' ||
-        dataRating === '2' ||
-        dataRating === '1'
-            ? ''
-            : 'rating_star_grey'
-
-    // to map a list of the equipments
-    const dataEquipments = data.equipments.map((equipment, index) => {
-        return (
-            <li
-                key={`${equipment}${index}-`}
-                className="accommodation_equipments_li"
-            >
-                {equipment}
-            </li>
-        )
-    })
-
-    // to save and uptdate the state of the picture
-
+    // save and uptdate the state of the picture
     const [imgStatus, updateImgStatus] = useState(0)
 
     const goToNext = () =>
-        imgStatus >= data.pictures.length - 1
+        imgStatus >= accommodationData.pictures.length - 1
             ? updateImgStatus(0)
             : updateImgStatus(imgStatus + 1)
+
     const goToPrevious = () =>
         imgStatus <= 0
-            ? updateImgStatus(data.pictures.length - 1)
+            ? updateImgStatus(accommodationData.pictures.length - 1)
             : updateImgStatus(imgStatus - 1)
 
-    // to hide element when there is only one picture
-    const isOneImage = data.pictures.length <= 1 ? 'hide' : ''
+    // hide element when there is only one picture
+    const isOneImage = () =>
+        accommodationData.pictures.length <= 1 ? 'hide' : ''
 
     return (
         <div className="accommodation">
             <Header />
-            <div className="accommodation_container">
-                <div className="accommodation_img">
-                    <img
-                        class="accommodation_img"
-                        id={`accommodation_img-${imgStatus + 1}`}
-                        src={data.pictures[imgStatus]}
-                        alt={`logement photo ${imgStatus + 1}`}
-                    />
-                    <span className={isOneImage} id="img-status">
-                        {imgStatus + 1}/{data.pictures.length}
-                    </span>
-                    <span onClick={goToPrevious}>
-                        <i
-                            className={`fa-solid fa-angle-left ${isOneImage}`}
-                            title="naviguer vers l'image précédente"
-                        ></i>
-                    </span>
-                    <span onClick={goToNext}>
-                        <i
-                            className={`fa-solid fa-angle-right ${isOneImage}`}
-                            title="naviguer vers l'image suivante"
-                        ></i>
-                    </span>
-                </div>
-
-                <div className="accommodation_info">
-                    <div className="accommodation_main-info">
-                        <h1>{data.title}</h1>
-                        <p>{data.location}</p>
-                        <div className="accommodation_tags">{dataTags}</div>
+            {accommodationData && (
+                <div className="accommodation_container">
+                    <div className="accommodation_img">
+                        <img
+                            className="accommodation_img"
+                            id={`accommodation_img-${imgStatus + 1}`}
+                            src={accommodationData.pictures[imgStatus]}
+                            alt={`logement photo ${imgStatus + 1}`}
+                        />
+                        <span className={isOneImage} id="img-status">
+                            {imgStatus + 1}/{accommodationData.pictures.length}
+                        </span>
+                        <span onClick={goToPrevious}>
+                            <i
+                                className={`fa-solid fa-angle-left ${isOneImage}`}
+                                title="naviguer vers l'image précédente"
+                            ></i>
+                        </span>
+                        <span onClick={goToNext}>
+                            <i
+                                className={`fa-solid fa-angle-right ${isOneImage}`}
+                                title="naviguer vers l'image suivante"
+                            ></i>
+                        </span>
                     </div>
-                    <div className="accommodation_host-rating">
-                        <div className="accommodation_host">
-                            <span>{data.host.name}</span>
-                            <div className="accommodation_host_img">
-                                <img
-                                    src={data.host.picture}
-                                    alt={data.host.name}
-                                />
+
+                    <div className="accommodation_info">
+                        <div className="accommodation_main-info">
+                            <h1>{accommodationData.title}</h1>
+                            <p>{accommodationData.location}</p>
+                            <div className="accommodation_tags">
+                                {renderTags()}
                             </div>
                         </div>
+                        <div className="accommodation_host-rating">
+                            <div className="accommodation_host">
+                                <span>{accommodationData.host.name}</span>
+                                <div className="accommodation_host_img">
+                                    <img
+                                        src={accommodationData.host.picture}
+                                        alt={accommodationData.host.name}
+                                    />
+                                </div>
+                            </div>
 
-                        <div
-                            className="accommodation_rating"
-                            title={`note du logement ${data.rating}/5`}
-                        >
-                            {[...Array(5).keys()].map((n)=>{
-                                if(n+1 <= data.rating){
-                                    <Rating isGrey='' key={n} />
-                                }else{
-                                    <Rating isGrey='rating_star_grey' key={n}/>
-                                }
-            
-                            })}
-                            {/* <Rating isGrey={star1} />
-                            <Rating isGrey={star2} />
-                            <Rating isGrey={star3} />
-                            <Rating isGrey={star4} />
-                            <Rating isGrey={star5} /> */}
+                            <div
+                                className="accommodation_rating"
+                                title={`note du logement ${accommodationData.rating}/5`}
+                            >
+                                {[...Array(5).keys()].map((n) => (
+                                    <Rating
+                                        key={n}
+                                        isGrey={
+                                            n + 1 > accommodationData.rating
+                                                ? 'rating_star_grey'
+                                                : ''
+                                        }
+                                    />
+                                ))}
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="accommodation_collapse">
-                    <Collapse
-                        title="Description"
-                        description={<p>{data.description}</p>}
-                    />
-                    <Collapse
-                        title="Équipements"
-                        description={<ul>{dataEquipments}</ul>}
-                    />
+                    <div className="accommodation_collapse">
+                        <Collapse
+                            title="Description"
+                            description={<p>{accommodationData.description}</p>}
+                        />
+                        <Collapse
+                            title="Équipements"
+                            description={<ul>{renderEquipments()}</ul>}
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
