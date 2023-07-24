28ee649d7386def679903f458ca54677e14f26ec
UserWindowsXP <wwwebsurf@gmail.com>
2023-07-24 16:16:21 +0200

    Home all accommodation+component Card + JSON about

--- src/components/AccommodationList/AccommodationList.jsx
+++ src/components/AccommodationList/AccommodationList.jsx
@@ -1,24 +1,24 @@
-import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import './accommodationList.scss'
+import AccommodationCard from '../accommodationCard/accommodationCard'
 import { datas } from '../../datas/datas'
 
 export default function AccommodationList() {
     const [accommodationsDatas, setAccommodationsDatas] = useState([])
 
-    const [page, setPage] = useState(0)
-
-    function getPaginatedData(start, end){
-        if(datas){
-            const datasSliced = datas.slice(start, end)
+    // store the data sliced in the state if there is data
+    function getPaginatedData() {
+        if (datas) {
+            const datasSliced = datas.slice()
             setAccommodationsDatas(datasSliced)
-        }else{
+        } else {
             console.log('error : datas not found')
         }
     }
 
+    // call the function at the loading of the page
     useEffect(() => {
-        getPaginatedData(page, page+6)
+        getPaginatedData()
     }, [])
 
     return (
@@ -26,17 +26,11 @@ export default function AccommodationList() {
             <div className="accommodationList_grid">
                 {accommodationsDatas.map((data, i) => {
                     return (
-                        <Link
-                            key={data.id}
-                            id={`accommodationList_grid_item-${i + 1}`}
-                            to={`/accommodation/${data.id}`}
-                        >
-                            <div className="accommodationList_grid_item">
-                                <div className="grey-gradient"></div>
-                                <h2> {data.title}</h2>
-                                <img src={data.cover} alt={data.title} />
-                            </div>
-                        </Link>
+                        <AccommodationCard
+                            dataID={data.id}
+                            dataTitle={data.title}
+                            dataCover={data.cover}
+                        />
                     )
                 })}
             </div>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,8 @@
-import Banner from '../../components/banner/banner'
 import './about.scss'
+import Banner from '../../components/banner/banner'
 import Collapse from '../../components/collapse/collapse'
 import Header from '../../components/header/header'
+import {dataAbout} from '../../datas/data-about'
 
 export default function About() {
     return (
@@ -9,54 +10,17 @@ export default function About() {
             <Header isAboutPage={true}/> 
             <Banner title="" bannerClass="banner banner_about" />
             <div className="about_container">
-                <Collapse
-                    title="Fiabilité"
-                    description={
-                        <p>
-                            Les annonces postées sur Kasa garantissent une
-                            fiabilité totale. Les photos sont conformes aux
-                            logements, et toutes les informations sont
-                            régulièrement vérifiées par nos équipes.
-                        </p>
-                    }
-                />
-                <Collapse
-                    title="Respect"
-                    description={
-                        <p>
-                            La bienveillance fait partie des valeurs fondatrices
-                            de Kasa. Tout comportement discriminatoire ou de
-                            perturbation du voisinage entraînera une exclusion
-                            de notre plateforme.
-                        </p>
-                    }
-                />
-                <Collapse
-                    title="Service"
-                    description={
-                        <p>
-                            La bienveillance fait partie des valeurs fondatrices
-                            de Kasa. Tout comportement discriminatoire ou de
-                            perturbation du voisinage entraînera une exclusion
-                            de notre plateforme.
-                        </p>
-                    }
-                />
-                <Collapse
-                    title="Sécurité"
-                    description={
-                        <p>
-                            La sécurité est la priorité de Kasa. Aussi bien pour
-                            nos hôtes que pour les voyageurs, chaque logement
-                            correspond aux critères de sécurité établis par nos
-                            services. En laissant une note aussi bien à l'hôte
-                            qu'au locataire, cela permet à nos équipes de
-                            vérifier que les standards sont bien respectés. Nous
-                            organisons également des ateliers sur la sécurité
-                            domestique pour nos hôtes.
-                        </p>
-                    }
-                />
+                {dataAbout.map((data, i) => {
+                    return(
+                        <Collapse
+                        key={`${data.name}-${i}`}
+                        title={data.name}
+                        description={
+                            <p>{data.description}</p>
+                        }
+                    />
+                    )
+                })}
             </div>
         </div>
     )
